7f1b201b3f39f83ac48539eb029603d48dc6fd0a
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-06-23 16:26:55 +0200

    Add pretrained model loading in layers_weights_hist_example script

--- examples/layers_weights_hist_example.py
+++ examples/layers_weights_hist_example.py
@@ -1,1 +1,11 @@
-from pytorch-debug-tools.LayersHistogramsVisualizer import LayersHistogramsVisualizer
+import torchvision
+from torchvision.models import resnet50
+
+from pytorch_debug_tools.LayersHistogramsVisualizer import LayersHistogramsVisualizer
+
+if __name__ == "__main__":
+    # check layers weights histograms for pretrained model
+    if torchvision.__version__ < '0.13.0':
+        pretrained_resnet50 = resnet50(pretrained=True)
+    else:
+        resnet50(weights="IMAGENET1K_V2")
